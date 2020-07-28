5ff1fcc5bcacae9c4a2be72aaa5d9fb5dc1d1424
Ubuntu <ubuntu@ip-172-31-6-7.cn-northwest-1.compute.internal>
2020-07-28 17:53:04 +0800

    增加dag_manager插件的使用

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM apache/airflow:1.10.11
 
 USER root
 
+COPY script/requirements.txt /requirements.txt
 COPY script/entrypoint_wrapper.sh /entrypoint_wrapper.sh
 COPY config/airflow.cfg ${AIRFLOW_HOME}/airflow.cfg
 COPY src/ /home/airflow/.local/lib/python3.6/site-packages/airflow/
--- plugins/dcmp/tools/upgradedb.py
+++ plugins/dcmp/tools/upgradedb.py
@@ -3,7 +3,7 @@
 import os
 
 from airflow import settings
-from airflow.hooks.mysql_hook import MySqlHook
+from airflow.hooks.postgres_hook import PostgresHook
 from airflow.hooks.base_hook import CONN_ENV_PREFIX
 
 
@@ -12,7 +12,7 @@ MYSQL_CONN_ID = "dag_creation_manager_plugin_sql_alchemy_conn"
 
 def get_mysql_hook():
     os.environ[CONN_ENV_PREFIX + MYSQL_CONN_ID.upper()] = settings.SQL_ALCHEMY_CONN
-    return MySqlHook(mysql_conn_id=MYSQL_CONN_ID)
+    return PostgresHook(postgres_conn_id=MYSQL_CONN_ID)
 
 
 def run_sql(sql, ignore_error=False):
@@ -30,89 +30,104 @@ def run_sql(sql, ignore_error=False):
 
 def run_version_0_0_1():
     run_sql("""
-        CREATE TABLE IF NOT EXISTS `dcmp_dag` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `dag_name` varchar(100) NOT NULL,
-          `version` int(11) NOT NULL,
-          `category` varchar(50) NOT NULL,
-          `editing` tinyint(1) NOT NULL,
-          `editing_user_id` int(11) DEFAULT NULL,
-          `editing_user_name` varchar(100) DEFAULT NULL,
-          `last_editor_user_id` int(11) DEFAULT NULL,
-          `last_editor_user_name` varchar(100) DEFAULT NULL,
-          `updated_at` datetime(6) NOT NULL,
-          PRIMARY KEY (`id`),
-          UNIQUE KEY `dag_name` (`dag_name`),
-          KEY `category` (`category`),
-          KEY `editing` (`editing`),
-          KEY `updated_at` (`updated_at`)
-        ) DEFAULT CHARSET=utf8mb4;
-    """)
+        CREATE TABLE dcmp_dag (
+          id serial NOT NULL,
+          dag_name varchar(100) NOT NULL,
+          version int NOT NULL,
+          category varchar(50) NOT NULL,
+          editing boolean NOT NULL,
+          editing_user_id int DEFAULT NULL,
+          editing_user_name varchar(100) DEFAULT NULL,
+          last_editor_user_id int DEFAULT NULL,
+          last_editor_user_name varchar(100) DEFAULT NULL,
+          updated_at timestamp NOT NULL,
+          approved_version int not NULL,
+          approver_user_id int not NULL,
+          approver_user_name varchar(100) DEFAULT NULL,
+          last_approved_at timestamp,
+          editing_start timestamp,
+
+          PRIMARY KEY (id),
+          constraint dcmp_dag_dag_name_unique unique(dag_name) 
+        ) ;
+        CREATE INDEX dcmp_dag_category_index ON dcmp_dag   (category);
+        CREATE INDEX dcmp_dag_editing_index ON dcmp_dag   (editing);
+        CREATE INDEX dcmp_dag_updated_at_index ON dcmp_dag   (updated_at);
+        CREATE INDEX dcmp_dag_editing_start_index ON dcmp_dag   (editing_start);
+        CREATE INDEX dcmp_dag_last_edited_at_index ON dcmp_dag   (last_edited_at);
+    """, ignore_error=True)
 
     run_sql("""
-        CREATE TABLE IF NOT EXISTS `dcmp_dag_conf` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `dag_id` int(11) NOT NULL,
-          `dag_name` varchar(100) NOT NULL,
-          `action` varchar(50) NOT NULL,
-          `version` int(11) NOT NULL,
-          `conf` text NOT NULL,
-          `creator_user_id` int(11) DEFAULT NULL,
-          `creator_user_name` varchar(100) DEFAULT NULL,
-          `created_at` datetime(6) NOT NULL,
-          PRIMARY KEY (`id`),
-          KEY `dag_id` (`dag_id`),
-          KEY `dag_name` (`dag_name`),
-          KEY `action` (`action`),
-          KEY `version` (`version`),
-          KEY `created_at` (`created_at`)
-        ) DEFAULT CHARSET=utf8mb4;
-    """)
+        CREATE TABLE  dcmp_dag_conf (
+          id serial NOT NULL,
+          dag_id int NOT NULL,
+          dag_name varchar(100) NOT NULL,
+          action varchar(50) NOT NULL,
+          version int NOT NULL,
+          conf text NOT NULL,
+          creator_user_id int DEFAULT NULL,
+          creator_user_name varchar(100) DEFAULT NULL,
+          created_at timestamp NOT NULL,
+          approver_user_id int DEFAULT NULL,
+          approver_user_name varchar(100) DEFAULT NULL,
+          approved_at timestamp,
+          last_edited_at timestamp,
+          PRIMARY KEY (id)
+        ) ;
+        CREATE INDEX dcmp_dag_conf_dag_id_index ON dcmp_dag_conf   (dag_id);
+        CREATE INDEX dcmp_dag_conf_dag_name_index ON dcmp_dag_conf   (dag_name);
+        CREATE INDEX dcmp_dag_conf_action_index ON dcmp_dag_conf   (action);
+        CREATE INDEX dcmp_dag_conf_version_index ON dcmp_dag_conf   (version);
+        CREATE INDEX dcmp_dag_conf_created_at_index ON dcmp_dag_conf   (created_at);
+    """, ignore_error=True)
 
 
 def run_version_0_0_2():
-    run_sql("ALTER TABLE dcmp_dag ADD editing_start datetime(6);", ignore_error=True)
-    run_sql("ALTER TABLE dcmp_dag ADD INDEX editing_start (editing_start);", ignore_error=True)
-    run_sql("ALTER TABLE dcmp_dag ADD last_edited_at datetime(6);", ignore_error=True)
-    run_sql("ALTER TABLE dcmp_dag ADD INDEX last_edited_at (last_edited_at);", ignore_error=True)
+    pass
+    run_sql("ALTER TABLE dcmp_dag ADD editing_start timestamp;", ignore_error=True)
+    run_sql("CREATE INDEX dcmp_dag_editing_start_index ON dcmp_dag   (editing_start);", ignore_error=True)
+    run_sql("ALTER TABLE dcmp_dag ADD last_edited_at timestamp;", ignore_error=True)
+    run_sql("CREATE INDEX dcmp_dag_last_edited_at_index ON dcmp_dag   (last_edited_at);", ignore_error=True)
 
 
 def run_version_0_1_1():
-    run_sql("ALTER TABLE dcmp_dag_conf CHANGE conf conf mediumtext NOT NULL;")
+    pass
 
 
 def run_version_0_2_0():
     run_sql("""
-        CREATE TABLE IF NOT EXISTS `dcmp_user_profile` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `user_id` int(11) NOT NULL,
-          `is_superuser` tinyint(1) NOT NULL,
-          `is_data_profiler` tinyint(1) NOT NULL,
-          `is_approver` tinyint(1) NOT NULL,
-          `updated_at` datetime(6) NOT NULL,
-          `created_at` datetime(6) NOT NULL,
-          PRIMARY KEY (`id`),
-          KEY `user_id` (`user_id`),
-          KEY `is_superuser` (`is_superuser`),
-          KEY `is_data_profiler` (`is_data_profiler`),
-          KEY `is_approver` (`is_approver`),
-          KEY `updated_at` (`updated_at`),
-          KEY `created_at` (`created_at`)
-        ) DEFAULT CHARSET=utf8mb4;
-    """)
+        CREATE TABLE dcmp_user_profile (
+          id serial NOT NULL,
+          user_id int NOT NULL,
+          is_superuser bool NOT NULL,
+          is_data_profiler bool NOT NULL,
+          is_approver bool NOT NULL,
+          updated_at timestamp NOT NULL,
+          created_at timestamp NOT NULL,
+          approval_notification_emails text NOT NULL,
+          PRIMARY KEY (id)
+        );
+        CREATE INDEX dcmp_user_profile_user_id_index ON dcmp_user_profile   (user_id);
+        CREATE INDEX dcmp_user_profile_is_superuser_index ON dcmp_user_profile   (is_superuser);
+        CREATE INDEX dcmp_user_profile_is_data_profiler_index ON dcmp_user_profile   (is_data_profiler);
+        CREATE INDEX dcmp_user_profile_is_approver_index ON dcmp_user_profile   (is_approver);
+        CREATE INDEX dcmp_user_profile_updated_at_index ON dcmp_user_profile   (updated_at);
+        CREATE INDEX dcmp_user_profile_created_at_index ON dcmp_user_profile   (created_at);
+    """, ignore_error=True)
 
 
 def run_version_0_2_1():
+    pass
     run_sql("ALTER TABLE dcmp_dag ADD approved_version int(11) NOT NULL;", ignore_error=True)
     run_sql("ALTER TABLE dcmp_dag ADD INDEX approved_version (approved_version);", ignore_error=True)
     run_sql("ALTER TABLE dcmp_dag ADD approver_user_id int(11) DEFAULT NULL;", ignore_error=True)
     run_sql("ALTER TABLE dcmp_dag ADD approver_user_name varchar(100) DEFAULT NULL;", ignore_error=True)
-    run_sql("ALTER TABLE dcmp_dag ADD last_approved_at datetime(6);", ignore_error=True)
+    run_sql("ALTER TABLE dcmp_dag ADD last_approved_at timestamp;", ignore_error=True)
     run_sql("ALTER TABLE dcmp_dag ADD INDEX last_approved_at (last_approved_at);", ignore_error=True)
 
     run_sql("ALTER TABLE dcmp_dag_conf ADD approver_user_id int(11) DEFAULT NULL;", ignore_error=True)
     run_sql("ALTER TABLE dcmp_dag_conf ADD approver_user_name varchar(100) DEFAULT NULL;", ignore_error=True)
-    run_sql("ALTER TABLE dcmp_dag_conf ADD approved_at datetime(6);", ignore_error=True)
+    run_sql("ALTER TABLE dcmp_dag_conf ADD approved_at timestamp;", ignore_error=True)
     run_sql("ALTER TABLE dcmp_dag_conf ADD INDEX approved_at (approved_at);", ignore_error=True)
     
     run_sql("ALTER TABLE dcmp_user_profile ADD approval_notification_emails text NOT NULL;", ignore_error=True)
